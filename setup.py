2f2cfb8ffe906f2bb56c3fcc6a35d09ccc960b50
Khalid Chakhmoun <fr.ckhalid@gmail.com>
2017-09-24 22:50:24 +0200

    clean-up

--- setup.py
+++ setup.py
@@ -35,11 +35,6 @@ setup(
     author_email='fr.ckhalid@gmail.com',
     url='https://github.com/KhalidCK/hwfact',
     packages=find_packages(include=['hwfact']),
-    entry_points={
-        'console_scripts': [
-            'hwfact=hwfact.cli:main'
-        ]
-    },
     include_package_data=True,
     install_requires=requirements,
     zip_safe=False,
@@ -48,13 +43,7 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements,
